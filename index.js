dbdc188512a8f4dc5e316e116259f4096c7acd3f
NAHUEL-42 <NIEVAS.NAHUEL.1998@GMAIL.COM>
2026-03-13 09:56:20 -0300

    hol

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
+
 const server = http.createServer((req, res) => {
-const envVar = process.env.PORT || 'Variable no definida';
-res.end(`Hola desde Azure App Service 🚀\nMY_ENV_VAR: ${envVar}`);
+	const envVar = process.env.PORT || 'Variable no definida';
+	const ambiente = process.env.ambiente || 'Variable ambiente no definida';
+	res.end(`Hola desde Azure App Service 🚀\nMY_ENV_VAR: ${envVar}\nambiente: ${ambiente}`);
 });
 
 server.listen(process.env.PORT || 3000);
